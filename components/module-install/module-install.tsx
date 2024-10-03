7d6f8de92bca644a4de9ae4b111f11cfb8af683d
Baran <me@baran.wang>
2024-10-03 13:20:40 +0000

    chore: code commit

--- components/module-install/module-install.tsx
+++ components/module-install/module-install.tsx
@@ -3,48 +3,126 @@ import { Tabs, Tab } from 'rspress/theme';
 
 import styles from './module-install.module.scss';
 
-import IconLoon from './icons/loon.webp';
-import IconSurge from './icons/surge.webp';
-import IconQx from './icons/qx.webp';
-import IconStash from './icons/stash.webp';
-import IconEgern from './icons/egern.webp';
-import IconShadowrocket from './icons/shadowrocket.webp';
+type SupportedApp = 'loon' | 'surge' | 'qx' | 'stash' | 'egern' | 'shadowrocket';
 
+const labelTextMap: Record<SupportedApp, string> = {
+  loon: 'Loon',
+  surge: 'Surge',
+  qx: 'Quantumult X',
+  stash: 'Stash',
+  egern: 'Egern',
+  shadowrocket: 'Shadowrocket',
+};
+
+const TabLabel: React.FC<{ type: SupportedApp }> = ({ type }) => (
+  <div className={styles.label}>
+    <div className={[styles.icon, styles[`icon-${type}`]].join(' ')} />
+    {labelTextMap[type]}
+  </div>
+);
+
+const urlTemplateMap: Record<SupportedApp, null | ((url: string) => string)> = {
+  loon: (url) => {
+    const result = new URL('https://www.nsloon.com/openloon/import');
+    result.searchParams.set('plugin', url);
+    return result.toString();
+  },
+  surge: (url) => {
+    const result = new URL('surge:///install-module');
+    result.searchParams.set('url', url);
+    return result.toString();
+  },
+  qx: (url) => {
+    const result = new URL('quantumult-x:///add-resource');
+    result.searchParams.set('remote-resource', JSON.stringify({ rewrite_remote: [url] }));
+    return result.toString();
+  },
+  stash: (url) => {
+    return `https://link.stash.ws/install-override/${url.replace('^https?://', '')}`;
+  },
+  egern: null,
+  shadowrocket: (url) => {
+    const result = new URL('shadowrocket://install');
+    result.searchParams.set('module', url);
+    return result.toString();
+  },
+};
+
+const TabContent: React.FC<{ type: SupportedApp; url: string }> = ({ type, url }) => {
+  const urlTemplate = urlTemplateMap[type];
+  const urlToOpen = urlTemplate ? urlTemplate(url) : url;
+  return (
+    <>
+      {url}
+      <h3>一键安装</h3>
+      {urlToOpen && <a href={urlToOpen}>一键安装</a>}
+    </>
+  );
+};
 
 export interface ModuleInstallProps {
   urls: {
-    loon?: string;
-    surge?: string;
-    qx?: string;
-    stash?: string;
-    egern?: string;
-    shadowrocket?: string;
+    [key in SupportedApp]?: string;
   };
   urlPrefix?: string;
 }
 
-const TabLabel: React.FC<{ icon: string; label: string }> = ({ icon, label }) => (
-  <div className={styles['tab-label']}>
-    <img src={icon} alt='' />
-    {label}
-  </div>
-);
-
 export const ModuleInstall: React.FC<ModuleInstallProps> = ({ urlPrefix = '', urls }) => {
   const values = useMemo(() => {
     const result = [];
-    if (urls.loon) result.push(<TabLabel label='Loon' icon={IconLoon} />);
-    if (urls.surge) result.push(<TabLabel label='Surge' icon={IconSurge} />);
-    if (urls.qx) result.push(<TabLabel label='Quantumult X' icon={IconQx} />);
-    if (urls.stash) result.push(<TabLabel label='Stash' icon={IconStash} />);
-    if (urls.egern) result.push(<TabLabel label='Egern' icon={IconEgern} />);
-    if (urls.shadowrocket) result.push(<TabLabel label='Shadowrocket' icon={IconShadowrocket} />);
+    if (urls.loon) result.push(<TabLabel type="loon" />);
+    if (urls.surge) result.push(<TabLabel type="surge" />);
+    if (urls.qx) result.push(<TabLabel type="qx" />);
+    if (urls.stash) result.push(<TabLabel type="stash" />);
+    if (urls.egern) result.push(<TabLabel type="egern" />);
+    if (urls.shadowrocket) result.push(<TabLabel type="shadowrocket" />);
     return result;
   }, [urls]);
+
+  const renderContent = useMemo(() => {
+    const result = [];
+    if (urls.loon)
+      result.push(
+        <Tab key="loon">
+          <TabContent type="loon" url={`${urlPrefix}${urls.loon}`} />
+        </Tab>,
+      );
+    if (urls.surge)
+      result.push(
+        <Tab key="surge">
+          <TabContent type="surge" url={`${urlPrefix}${urls.surge}`} />
+        </Tab>,
+      );
+    if (urls.qx)
+      result.push(
+        <Tab key="qx">
+          <TabContent type="qx" url={`${urlPrefix}${urls.qx}`} />
+        </Tab>,
+      );
+    if (urls.stash)
+      result.push(
+        <Tab key="stash">
+          <TabContent type="stash" url={`${urlPrefix}${urls.stash}`} />
+        </Tab>,
+      );
+    if (urls.egern)
+      result.push(
+        <Tab key="egern">
+          <TabContent type="egern" url={`${urlPrefix}${urls.egern}`} />
+        </Tab>,
+      );
+    if (urls.shadowrocket)
+      result.push(
+        <Tab key="shadowrocket">
+          <TabContent type="shadowrocket" url={`${urlPrefix}${urls.shadowrocket}`} />
+        </Tab>,
+      );
+    return result;
+  }, [urlPrefix, urls]);
+
   return (
-    <Tabs groupId='module.install' values={values}>
-      <Tab key='install'>Install</Tab>
-      <Tab key='usage'>Usage</Tab>
+    <Tabs groupId="module.install" values={values}>
+      {renderContent}
     </Tabs>
   );
 };

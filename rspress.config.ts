a9062a542c3b948f38edbcc76768cfb9c2db7d76
Baran <me@baran.wang>
2024-10-03 02:44:33 +0000

    init

---
+++ rspress.config.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'iRingo',
+  description: '解锁完整的  Apple 功能和集成服务',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/NSRingo' },
+    ],
+  },
+});

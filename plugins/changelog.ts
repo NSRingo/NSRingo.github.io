fee712afa59ab8f44daa3a39fc1a571762187697
baranwang <me@baran.wang>
2024-10-08 14:43:04 +0800

    fix: sidebar 丢失的问题

--- plugins/changelog.ts
+++ plugins/changelog.ts
@@ -20,6 +20,9 @@ export function changelogPlugin({ baseRoutePath = 'changelog', repos = [] }: Cha
         try {
           let data = [];
           process.stdout.write(`Fetching releases for ${repo.name}...`);
+
+          let content = `---\ntitle: ${repo.name} - 更新日志\n---\n# ${repo.name}`;
+
           if (needFetch) {
             const res = await fetch(`https://api.github.com/repos/${repo.repo}/releases`);
             process.stdout.write(' - done\n');
@@ -29,8 +32,9 @@ export function changelogPlugin({ baseRoutePath = 'changelog', repos = [] }: Cha
             }
           } else {
             process.stdout.write(' - skipped\n');
+            content += "\n\n:::tip\n开发环境默认不拉取 Changelog，可指定 `NODE_ENV='production'` 强制开启\n:::";
           }
-          let content = `---\ntitle: ${repo.name} - 更新日志\n---\n# ${repo.name}`;
+
           if (data?.length) {
             for (const release of data) {
               content += '\n\n';
@@ -51,11 +55,13 @@ export function changelogPlugin({ baseRoutePath = 'changelog', repos = [] }: Cha
       return pages;
     },
     config(config) {
+      console.log(config);
       return {
         ...config,
         themeConfig: {
           ...config.themeConfig,
           sidebar: {
+            ...config.themeConfig?.sidebar,
             [`/${baseRoutePath}/`]: repos.map((repo) => ({
               text: repo.name,
               link: `${baseRoutePath}/${repo.path}`,

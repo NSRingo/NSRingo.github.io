305abd665bb6ed795ee98cfb8af87b9141a87db2
脆薯饼 <Virgil@nanocat.me>
2026-07-28 17:45:37 +0800

    fix(nav): 保留自动导航配置

--- rspress.config.ts
+++ rspress.config.ts
@@ -63,6 +63,7 @@ export default defineConfig({
     }),
     pluginChangelog({
       fetchOnDev: false,
+      addSidebar: false,
       items: generateChangelogParams([
         {
           title: "🌤 WeatherKit",
